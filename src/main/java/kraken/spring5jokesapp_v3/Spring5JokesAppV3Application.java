60882679f02dc576710c2ddbdc37e3c660e3d0f9
Pavlo Vovnyuk <pavlo.vovnyuk@capgemini.com>
2023-02-08 14:54:09 +0100

    my commit

---
+++ src/main/java/kraken/spring5jokesapp_v3/Spring5JokesAppV3Application.java
@@ -0,0 +1,13 @@
+package kraken.spring5jokesapp_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppV3Application.class, args);
+    }
+
+}
